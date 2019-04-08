316ba55a72570feb91cd9a68900b6b8051a5b8e1
AceWall <wallacearts@hotmail.com>
2019-04-08 01:26:08 -0300

    Some tweeks in the unittest to better test the cases.

--- test_BalancedBSTSet.py
+++ test_BalancedBSTSet.py
@@ -4,18 +4,18 @@ from BalancedBSTSet import BalancedBSTSet
 
 class TestBalancedBSTSet(unittest.TestCase):
     def setUp(self):
-        self.bst = BalancedBSTSet()
-        self.r = self.bst.add(5)
-        self.a = self.bst.add(3)
-        self.b = self.bst.add(7)
-        self.ba = self.bst.add(6)
-        self.bb = self.bst.add(8)
+        self.bst = BalancedBSTSet(False)
+        self.bst.add(3)
+        self.bst.add(1)
+        self.bst.add(7)
+        self.bst.add(6)
+        self.bst.add(8)
 
-    def test_children(self):
-        self.assertEqual(self.r.left, self.a)
-        self.assertEqual(self.r.right, self.b)
-        self.assertEqual(self.b.left, self.ba)
-        self.assertEqual(self.b.right, self.bb)
+    def test_structure(self):
+        self.assertEqual(self.bst.root.data, 3)
+        self.assertEqual(self.bst.root.left.data, 1)
+        self.assertEqual(self.bst.root.right.left.data, 6)
+        self.assertEqual(self.bst.root.right.right.data, 8)
 
 
 if __name__ == '__main__':
